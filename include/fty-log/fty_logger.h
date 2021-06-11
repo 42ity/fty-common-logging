879e186a5d6861bdb36a1c1846f0c8dfe25d1c10
Dmitrij Zukov <DmitrijZukov@eaton.com>
2021-06-11 17:14:39 +0200

    Added cxx loging macro

--- include/fty-log/fty_logger.h
+++ include/fty-log/fty_logger.h
@@ -107,7 +107,41 @@
 
 //  @interface
 #ifdef __cplusplus
+#include <fmt/format.h>
 // Log class
+
+#define logError(...)\
+    fmtlog(log4cplus::ERROR_LOG_LEVEL, __VA_ARGS__)
+
+#define logDebug(...)\
+    fmtlog(log4cplus::DEBUG_LOG_LEVEL, __VA_ARGS__)
+
+#define logInfo(...)\
+    fmtlog(log4cplus::INFO_LOG_LEVEL, __VA_ARGS__)
+
+#define logWarn(...)\
+    fmtlog(log4cplus::WARN_LOG_LEVEL, __VA_ARGS__)
+
+#define logFatal(...)\
+    fmtlog(log4cplus::FATAL_LOG_LEVEL, __VA_ARGS__)
+
+#define fmtlog(level, ...) \
+    ftylog_getInstance()->insertLog(level, __FILE__, __LINE__, __func__, fty::logger::format(__VA_ARGS__).c_str())
+
+namespace fty::logger {
+
+inline std::string format(const std::string& str)
+{
+    return str;
+}
+
+template<typename... Args>
+inline std::string format(const std::string& str, const Args&... args)
+{
+    return fmt::format(str, args...);
+}
+
+}
 
 class Ftylog
 {
