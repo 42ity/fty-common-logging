ca351665f03fd6b985f83f9552dd7ab7a06b6a2a
Clément <perrettecl@users.noreply.github.com>
2021-10-12 11:58:57 +0200

    Avoid log error in production

--- include/fty-log/fty_logger.h
+++ include/fty-log/fty_logger.h
@@ -141,7 +141,19 @@ inline std::string format(const std::string& str)
 template<typename... Args>
 inline std::string format(const std::string& str, const Args&... args)
 {
-    return fmt::format(str, args...);
+    //fmt format can thow exception
+    //In debug mode we want to take benefits of those exception to solve the formating issue
+    //However that's not a good idea for production
+    std::string returnStr;
+    try {
+        returnStr = fmt::format(str, args...);
+    } catch(std::exception& e) {
+#ifdef DEBUG
+        throw;
+#endif
+        returnStr = str + " /!\ A log serialization error happened: " + e.what();
+    }
+    return returnStr;
 }
 
 }
